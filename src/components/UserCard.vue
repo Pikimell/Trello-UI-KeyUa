8d472229ab1ed8699f8dab8bc451a2507552569d
unknown <pikimelposhta@gmail.com>
2021-11-28 18:03:04 +0200

    Header1

---
+++ src/components/UserCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>
+    <b-avatar class="m-1" src="https://placekitten.com/300/300" variant="primary" text="BV" size="6rem"></b-avatar>
+    <h3 >{{msg}}</h3>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style>
+
+</style>
