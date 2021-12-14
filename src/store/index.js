076db1d0dbcee1ac88fd97e771f8fc7f218cf73d
unknown <pikimelposhta@gmail.com>
2021-12-14 02:10:54 +0200

    Start

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue"
 import Vuex from 'vuex'
 
+const axios = require('axios');
+const ENDPOINT = "http://localhost:3000";
+
+
 Vue.use(Vuex)
 
 
@@ -10,6 +14,12 @@ export default new Vuex.Store({
         cards: []
     },
     mutations: {
+        loadColumns: (state, columns) => {
+            state.cols = columns;
+        },
+        loadCards: (state, cards) => {
+            state.cards = cards;
+        },
         pushColumn: (state, column) => {
             state.cols.push(column);
         },
@@ -20,6 +30,7 @@ export default new Vuex.Store({
             function isPrime(column) {
                 return column.idColumn === idColumn
             }
+
             let index = state.cols.findIndex(isPrime);
             state.cols.splice(index, 1)
         },
@@ -27,6 +38,7 @@ export default new Vuex.Store({
             function isPrime(card) {
                 return card.idCard === idCard
             }
+
             let index = state.cards.findIndex(isPrime);
             state.cards.splice(index, 1)
         },
@@ -56,22 +68,87 @@ export default new Vuex.Store({
         }
     },
     actions: {
+        loadColumns: ({commit}) => {
+            axios.get(ENDPOINT + '/getColumns')
+                .then(function (response) {
+                    commit('loadColumns', response.data.Items.reverse())
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .then(function () {
+                });
+        },
+        loadCards: ({commit}) => {
+            axios.get(ENDPOINT + '/getCards')
+                .then(function (response) {
+                    commit('loadCards', response.data.Items.reverse())
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .then(function () {
+                });
+        },
+
+
         pushColumn: ({commit}, column) => {
-            commit('pushColumn', column)
+            axios.post(ENDPOINT + '/pushColumn', {
+                idColumn: column.idColumn,
+                title: column.title
+            }).then(function (response) {
+                if (response.statusText === "OK")
+                    commit('pushColumn', column);
+            }).catch(function (error) {
+                console.log(error);
+            });
+
         },
         pushCard: ({commit}, card) => {
-            commit('pushCard', card)
+            axios.post(ENDPOINT + '/pushCard', {
+                idCard: card.idCard,
+                idColumn: card.idColumn,
+                title: card.title,
+                description: card.description
+            }).then(function (response) {
+                if (response.statusText === "OK")
+                    commit('pushCard', card)
+            }).catch(function (error) {
+                console.log(error);
+            });
         },
         delColumn: ({commit}, idColumn) => {
-            commit('delColumn', idColumn)
+            axios.delete(ENDPOINT + `/deleteColumn/${idColumn}`).then(function (response) {
+                if (response.statusText === "OK") {
+                    commit('delColumn', idColumn)
+
+                    //TODO delete cards of columns
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
         },
         delCard: ({commit}, idCard) => {
-            commit('delCard', idCard)
+            axios.delete(ENDPOINT + `/deleteCard/${idCard}`).then(function (response) {
+                if (response.statusText === "OK")
+                    commit('delCard', idCard)
+            }).catch(function (error) {
+                console.log(error);
+            });
+
         },
         editTitleCard: ({commit}, props) => {
             commit('editTitleCard', props)
         },
         editTitleCol: ({commit}, props) => {
+            axios.put(ENDPOINT + `/updateColumn/${props.idColumn}`, {
+                title: props.title
+            }).then(response => {
+                console.log(response);
+                commit('editTitleColumn', props)
+            }).catch(error => {
+                console.log(error);
+            });
             commit('editTitleCol', props)
         },
         editDescCard: ({commit}, props) => {
@@ -86,7 +163,5 @@ export default new Vuex.Store({
             return state.cards;
         }
     },
-    modules: {
-
-    }
+    modules: {}
 })
