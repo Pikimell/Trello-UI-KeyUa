15dd09675076ee5c7a690365afa6c2aaa6ec0154
Пащенко Владимир <94864615+Pikimell@users.noreply.github.com>
2021-12-24 17:37:30 +0200

    Build Spec 10

--- src/store/consts.js
+++ src/store/consts.js
@@ -1,5 +1,5 @@
-//const PATH = 'http://localhost:3000'
-const PATH = 'https://njjbnf9sh3.execute-api.us-east-1.amazonaws.com/dev'
+const PATH = 'http://localhost:3000'
+//const PATH = 'https://njjbnf9sh3.execute-api.us-east-1.amazonaws.com/dev'
 export {
     PATH
 }
