c81e0f205fb8c284ba5e20715a537407177e9ab2
unknown <pikimelposhta@gmail.com>
2022-01-21 05:59:56 +0200

    Update consts.js

--- src/store/consts.js
+++ src/store/consts.js
@@ -1,5 +1,4 @@
-//const PATH = 'https://826b4dafwc.execute-api.us-east-2.amazonaws.com/dev';
-const PATH = 'http://localhost:3000';
+const PATH = 'https://826b4dafwc.execute-api.us-east-2.amazonaws.com/dev';
 export {
     PATH
 }
